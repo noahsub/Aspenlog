e7b69d0d068079ea0c05e2ebe1c9f38ee0a61157
noahsub <48663769+noahsub@users.noreply.github.com>
2023-10-22 23:31:40 -0400

    Testing: Testing Fast API File output
    More details:
    - Converted table_c2_extraction's dataframe output to CSV and returned it over a GET endpoint

--- backend/test.py
+++ backend/test.py
@@ -1,6 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from fastapi.responses import FileResponse
 
 import NBCC_Data_Extraction
 
@@ -13,8 +14,8 @@ def read_root():
 
 @app.get("/table_c2")
 def table_c2():
-    return HTMLResponse(content=NBCC_Data_Extraction.table_c2_extraction().to_html(), status_code=200)
-
+    # return HTMLResponse(content=NBCC_Data_Extraction.table_c2_extraction().to_html(), status_code=200)
+    return FileResponse(path='/output/table_c2.csv', filename='table_c2.csv')
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=42613)
