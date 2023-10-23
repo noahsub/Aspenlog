79bc052b33bc6b3828e1d12bcbff6fa43985caf6
noahsub <48663769+noahsub@users.noreply.github.com>
2023-10-22 23:24:48 -0400

    Testing: Testing Fast API HTML output
    More details:
    - Converted table_c2_extraction's dataframe output to HTML and returned it over a GET endpoint
    - Moved files around for convenience

--- backend/test.py
+++ backend/test.py
@@ -1,12 +1,19 @@
-from fastapi import FastAPI
 import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+import NBCC_Data_Extraction
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"SEEDA-UOFT Backend": "Running"}
+
+@app.get("/table_c2")
+def table_c2():
+    return HTMLResponse(content=NBCC_Data_Extraction.table_c2_extraction().to_html(), status_code=200)
 
 
 if __name__ == "__main__":
