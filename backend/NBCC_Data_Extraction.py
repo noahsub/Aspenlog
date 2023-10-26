d925e1018ff96d26f587427a62c76b90c0ed22d2
noahsub <48663769+noahsub@users.noreply.github.com>
2023-10-26 00:10:43 -0400

    Fix: Too many rows were being missed by the regex alone
    More details:
    - Added a secondary regex to catch corner cases
    - Made some replacements in extracted text in order for regex to detect it
    - Any remaining errors must be fixed manually by user input

--- backend/NBCC_Data_Extraction.py
+++ backend/NBCC_Data_Extraction.py
@@ -1,4 +1,17 @@
 ########################################################################################################################
+# NBCC_Data_Extraction.py
+# This file contains functions that extract data from the National Building Code of Canada 2020 (NBCC2020) and store it
+# in a Pandas dataframe. The data is then converted to a csv file and an Excel file and saved in the output directory.
+# The data is extracted from the following tables:
+#   - Table C1: Design Wind Pressures for Components and Cladding
+#   - Table C2: Design Wind Pressures for Main Wind-Force Resisting Systems
+#
+# This code may not be reproduced, disclosed, or used without the specific written permission of the owners
+# Owners: University of Toronto, SEEDA
+# Author(s): https://github.com/noahsub
+########################################################################################################################
+
+########################################################################################################################
 # IMPORTS
 ########################################################################################################################
 import re
@@ -67,10 +80,10 @@ def dataframe_to_csv(dataframe: pd.DataFrame, filename: str) -> None:
 
 def dataframe_to_excel(dataframe: pd.DataFrame, filename: str) -> None:
     """
-    Convert Pandas dataframe to excel file and save it to the output directory.
+    Convert Pandas dataframe to Excel file and save it to the output directory.
 
     :param dataframe: The dataframe to save.
-    :param filename: The filename of the excel file (ensure to include the .xlsx extension).
+    :param filename: The filename of the Excel file (ensure to include the .xlsx extension).
     """
     assert ".xlsx" in filename
 
@@ -87,6 +100,7 @@ def table_c1_extraction() -> pd.DataFrame:
 
     :return: A Pandas dataframe containing the extracted data.
     """
+
     # The extracted text
     text = pdf_to_text("NBCC2020-Table-C-1.pdf")
 
@@ -96,12 +110,15 @@ def table_c1_extraction() -> pd.DataFrame:
 
     # Text is analyzed and lines deemed usable data are stored.
     processed = []
+    errors = []
     for page in text:
         for line in page:
             # Save data
             if data_regex.match(line):
                 # Convert to list of floats
                 processed.append([float(x) for x in line.split(' ')])
+            else:
+                errors.append(line)
 
     # Headers for the data
     layer_one_headers = ['q', 'V', 'q', 'V', 'q', 'V', 'q', 'V']
@@ -129,28 +146,47 @@ def table_c2_extraction() -> pd.DataFrame:
 
     :return: A Pandas dataframe containing the extracted data.
     """
-    # The extracted text
+
+    # The computer vision extracted text by page and line
     text = pdf_to_text("NBCC2020-Table-C-2.pdf")
 
-    # The regex used to extract data lines, in this case anything followed by 16 numbers separated by spaces
-    data_pattern = r".*\s(([-+]?[0-9]*\.?[0-9]+)\s){15}([-+]?[0-9]*\.?[0-9]+)"
-    data_regex = re.compile(data_pattern)
+    # This regular expression matches a string that ends with a sequence of 16 floating point numbers. The numbers
+    # are separated by spaces, with 15 of them followed by a space and the last one at the end of the string. The
+    # string before the numbers can be any characters.
+    # Example: 'Toronto 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16'
+    data_pattern_1 = r".*\s(([-+]?[0-9]*\.?[0-9]+)\s){15}([-+]?[0-9]*\.?[0-9]+)"
+    data_regex_1 = re.compile(data_pattern_1)
+
+    # This regular expression matches a string that starts with any characters (including digits within parentheses)
+    # followed by a sequence of 16 floating point numbers. The numbers are separated by spaces, with 15 of them
+    # followed by a space and the last one at the end of the string.
+    # Example: 'Vancouver(Granville St. & 41stAve) 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16'
+    data_pattern_2 = r".*\(([^)]*)\)\D*(([-+]?[0-9]*\.?[0-9]+\s){15}([-+]?[0-9]*\.?[0-9]+))"
+    data_regex_2 = re.compile(data_pattern_2)
 
     # Text is analyzed and lines deemed usable data are stored.
     processed = []
+    errors = []
     for page in text:
         for line in page:
+            line = line.replace('- ', ' -')
+            line = line.replace(')', ') ')
             # Save location
             if "Region" in line or line in CANADIAN_PROVINCES_AND_TERRITORIES:
                 processed.append([line] + [""] * 16)
             # Save data
-            elif data_regex.match(line):
+            elif data_regex_1.match(line) or data_regex_2.match(line):
                 # Separate numerical data from the location
                 data = line.split(' ')
                 numerical_data = [float(x) for x in data[-16:]]
                 location = ' '.join(data[:-16])
                 numerical_data.insert(0, location)
                 processed.append(numerical_data)
+            else:
+                if len(line.split(' ')) > 10 and "Division" not in line and "National Research" not in line:
+                    errors.append(line)
+
+    print(len(errors))
 
     # Headers for the data
     layer_one_headers = ["Province and Location", "Elev., m", "Design Temperature", "", "", "",
