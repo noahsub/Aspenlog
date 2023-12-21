6479e6b57ee477bb311505147d76314c8c7e71be
noahsub <48663769+noahsub@users.noreply.github.com>
2023-12-21 16:29:36 -0500

    Feature: Finished on populate_climatic_data_table(), all data from table_c2.csv has successfully been copied over to the database.
    More details:
    - Parsed mismatched data types using a combination of conditions and an abstract syntax tree.

--- database/Population/populate_climate_data.py
+++ database/Population/populate_climate_data.py
@@ -1,8 +1,7 @@
+import ast
 import csv
-
 from sqlalchemy import inspect
 from sqlalchemy.orm import sessionmaker
-
 from config import get_file_path
 from database.Constants.connection_constants import PrivilegeType
 from database.Entities.climatic_data import BASE, ClimaticData
@@ -37,11 +36,50 @@ def populate_climatic_data_table():
         for _ in range(3):
             next(csv_file)
 
+        # read data-extraction/output/table_c2.csv file
         csv_reader = csv.reader(csv_file)
 
+        # map column names to appropriate column index
         for row in csv_reader:
-            entry = ClimaticData(ProvinceAndLocation=row[1])
+            column_mapping = {
+                'ProvinceAndLocation': 1,
+                'Elev_m': 2,
+                'Jan_2_5_percent_C': 3,
+                'Jan_1_percent_C': 4,
+                'July_Dry_C': 5,
+                'July_Wet_C': 6,
+                'DegreeDaysBelow18C': 7,
+                'Rain_15_Min_mm': 8,
+                'OneDayRain_1_50_mm': 9,
+                'Ann_Rain_mm': 10,
+                'MoistIndex': 11,
+                'Ann_Tot_Ppn_mm': 12,
+                'DrivingRainWindPressures_Pa_1_5': 13,
+                'SnowLoad_kPa_1_50_Ss': 14,
+                'SnowLoad_kPa_1_50_Sr': 15,
+                'HourlyWindPressures_kPa_1_10': 16,
+                'HourlyWindPressures_kPa_1_50': 17
+            }
+
+            # store the data of the entry in a dictionary
+            entry_data = {}
+            # go through each column and set value
+            for column in column_mapping:
+                # the only column that should be a string
+                if column == 'ProvinceAndLocation':
+                    entry_data[column] = row[column_mapping[column]]
+                # if an empty string is found as a value, we set that value to null in the database
+                elif row[column_mapping[column]] == '':
+                    entry_data[column] = None
+                # otherwise, we use an abstract syntax tree to parse the value appropriately
+                else:
+                    entry_data[column] = ast.literal_eval(row[column_mapping[column]])
+
+            # unpack entry_data to create a ClimaticData object
+            entry = ClimaticData(**entry_data, Latitude=0, Longitude=0)
+            # prepare the entry to be added to the database
             controller.add(entry)
+        # add all entries to the database
         controller.commit()
 
 
