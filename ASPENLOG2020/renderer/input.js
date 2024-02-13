d513160c4c411466d8d0c402467ba12668db3e2b
noahsub <48663769+noahsub@users.noreply.github.com>
2024-02-12 20:44:56 -0500

    Feature: Building parameters
    More Details:
    - Added cladding, dominant opening, and roof inputs
    - Finished height zone elevation tables

--- ASPENLOG2020/renderer/input.js
+++ ASPENLOG2020/renderer/input.js
@@ -1,3 +1,5 @@
+// import $ from 'jquery';
+
 // on window load
 var map;
 
@@ -10,20 +12,17 @@ window.onload = function() {
     toggleMenuColors('#site-designation-selection');
     toggleMenuColors('#eave-and-ridge-selection');
     toggleMenuColors('#number-height-zone-selection');
+    toggleMenuColors('#dominant-opening-selection');
 
 
     var siteDesignationSelectionOptions = document.querySelectorAll('#site-designation-selection .btn input');
-    for (var i = 0; i < siteDesignationSelectionOptions.length; i++)
-    {
-        siteDesignationSelectionOptions[i].addEventListener('change', function()
-        {
+    for (var i = 0; i < siteDesignationSelectionOptions.length; i++) {
+        siteDesignationSelectionOptions[i].addEventListener('change', function() {
             if (this.checked) {
-                if (this.id === 'xs_option')
-                {
+                if (this.id === 'xs_option') {
                     xsCase();
                 }
-                if (this.id === 'vs30_option')
-                {
+                if (this.id === 'vs30_option') {
                     vs30Case();
                 }
             }
@@ -31,48 +30,121 @@ window.onload = function() {
     }
 
     var eaveAndRidgeSelectionOptions = document.querySelectorAll('#eave-and-ridge-selection .btn input');
-    for (var i = 0; i < eaveAndRidgeSelectionOptions.length; i++)
-    {
-        eaveAndRidgeSelectionOptions[i].addEventListener('change', function()
-        {
-            if (this.id === 'eave-and-ridge-yes-option')
-            {
+    for (var i = 0; i < eaveAndRidgeSelectionOptions.length; i++) {
+        eaveAndRidgeSelectionOptions[i].addEventListener('change', function() {
+            if (this.id === 'eave-and-ridge-yes-option') {
                 eaveAndRidgeCase();
             }
-            if (this.id === 'eave-and-ridge-no-option')
-            {
+            if (this.id === 'eave-and-ridge-no-option') {
                 standardDimensionsCase();
             }
         });
     }
 
     var numberHeightZoneOptions = document.querySelectorAll('#number-height-zone-selection .btn input');
-    for (var i = 0; i < numberHeightZoneOptions.length; i++)
-    {
-        numberHeightZoneOptions[i].addEventListener('change', function()
-        {
-            if (this.id === 'number-height-zone-yes-option')
-            {
+    for (var i = 0; i < numberHeightZoneOptions.length; i++) {
+        numberHeightZoneOptions[i].addEventListener('change', function() {
+            if (this.id === 'number-height-zone-yes-option') {
                 defaultHeightZoneNumberCase();
             }
-            if (this.id === 'number-height-zone-no-option')
-            {
+            if (this.id === 'number-height-zone-no-option') {
                 nonDefaultHeightZoneNumberCase();
+            }
+        });
+    }
+
+    var dominantOpeningOptions = document.querySelectorAll('#dominant-opening-selection .btn input');
+    for (var i = 0; i < dominantOpeningOptions.length; i++) {
+        dominantOpeningOptions[i].addEventListener('change', function() {
+            if (this.id === 'dominant-opening-yes-option') {
+                dominantOpeningCase();
             }
         });
     }
 }
 
-function addRow() {
+function populateDefaultHeightZoneElevation()
+{
+    var height = null;
+
+    if (document.getElementById('eave-and-ridge-yes-option').checked)
+    {
+        var eaveHeight = parseFloat(document.getElementById('eave-height').value);
+        var ridgeHeight = parseFloat(document.getElementById('ridge-height').value);
+        height = (eaveHeight + ridgeHeight) / 2;
+    }
+
+    else if (document.getElementById('eave-and-ridge-no-option').checked)
+    {
+        height = document.getElementById('height').value;
+    }
+
+    if (height) {
+        var numHeightZones = Math.ceil(height / 20);
+        for (var j = 1; j < numHeightZones + 1; j++) {
+            addHeightZoneElevationRow(j * 20);
+        }
+    }
+}
+
+function clearHeightZoneElevationTable()
+{
+    var table = document.getElementById("height-zone-elevation-table");
+    while (table.rows.length !== 1)
+    {
+        table.deleteRow(-1);
+    }
+}
+
+function heightChange()
+{
+    if (document.getElementById('number-height-zone-yes-option').checked) {
+        console.log("attempting");
+        clearHeightZoneElevationTable();
+        populateDefaultHeightZoneElevation();
+    } else if (document.getElementById('number-height-zone-no-option').checked) {
+        // Your code here
+    }
+}
+
+function addHeightZoneElevationRow(elevation) {
     var table = document.getElementById("height-zone-elevation-table");
     var row = table.insertRow(-1);
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
-    cell1.innerHTML = table.rows.length - 1;
+    if (table.rows.length === 1)
+    {
+        cell1.innerHTML = 1;
+    }
+
+    else
+    {
+        cell1.innerHTML = table.rows.length - 1;
+    }
+    cell2.innerHTML = elevation;
+}
+
+function addHeightZoneElevationRowEditable()
+{
+    var table = document.getElementById("height-zone-elevation-table");
+    var row = table.insertRow(-1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    if (table.rows.length === 1)
+    {
+        cell1.innerHTML = 1;
+    }
+
+    else
+    {
+        cell1.innerHTML = table.rows.length - 1;
+    }
+    cell2.innerHTML = "0";
     cell2.contentEditable = "true";
 }
 
-function removeRow() {
+function removeHeightZoneElevationRow()
+{
     var table = document.getElementById("height-zone-elevation-table");
     if (table.rows.length > 2) {
         table.deleteRow(-1);
@@ -99,8 +171,7 @@ function setMap(latitude, longitude, address) {
 }
 
 
-function vs30Case()
-{
+function vs30Case() {
     fetch('subPages/vs30.html')
         .then(response => response.text())
         .then(data => {
@@ -111,8 +182,7 @@ function vs30Case()
         });
 }
 
-function xsCase()
-{
+function xsCase() {
     fetch('subPages/xs.html')
         .then(response => response.text())
         .then(data => {
@@ -124,8 +194,7 @@ function xsCase()
         });
 }
 
-function standardDimensionsCase()
-{
+function standardDimensionsCase() {
     fetch('subPages/standard_dimensions.html')
         .then(response => response.text())
         .then(data => {
@@ -137,8 +206,7 @@ function standardDimensionsCase()
         });
 }
 
-function eaveAndRidgeCase()
-{
+function eaveAndRidgeCase() {
     fetch('subPages/eave_and_ridge_dimensions.html')
         .then(response => response.text())
         .then(data => {
@@ -150,43 +218,45 @@ function eaveAndRidgeCase()
         });
 }
 
-function nonDefaultHeightZoneNumberCase()
-{
+function nonDefaultHeightZoneNumberCase() {
     fetch('subPages/non_default_height_zone_number.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('height-zone-elevation-container').innerHTML = data;
             toggleMenuColors('#number-height-zone-selection');
+            document.addEventListener('input', heightChange);
         })
         .catch((error) => {
             console.error('Error:', error);
         });
 }
 
-function defaultHeightZoneNumberCase()
-{
+function defaultHeightZoneNumberCase() {
     fetch('subPages/default_height_zone_number.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('height-zone-elevation-container').innerHTML = data;
             toggleMenuColors('#number-height-zone-selection');
-            var height = document.getElementById('height').value;
-            // if height is not null or empty string
-            if (height) {
-                var numHeightZones = Math.ceil(height / 20);
-                for (var j = 1; j < numHeightZones + 1; j++) {
-                    var table = document.getElementById("height-zone-elevation-table");
-                    var row = table.insertRow(-1);
-                    var cell1 = row.insertCell(0);
-                    var cell2 = row.insertCell(1);
-                    cell1.innerHTML = j + 1; // Height Zone starts from 1
-                    cell2.innerHTML = j * 20; // Elevation increases by 20 for each zone
-                }
-            }
+            clearHeightZoneElevationTable();
+            populateDefaultHeightZoneElevation();
+            document.addEventListener('input', heightChange);
         })
         .catch((error) => {
             console.error('Error:', error);
         });
+}
+
+function dominantOpeningCase() {
+    fetch('subPages/dominant_opening.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('dominant-opening-container').innerHTML = data;
+            toggleMenuColors('#dominant-opening-selection');
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+
 }
 
 function toggleMenuColors(toggleMenu) {
@@ -213,81 +283,69 @@ document.getElementById('location_button').addEventListener('click', function() 
     if (document.getElementById('vs30_option').checked) {
         siteDesignation = 'xv';
         seismicValue = document.getElementById('vs30').value;
-    }
-
-    else{
+    } else {
         if (document.getElementById('xs-A-option').checked) {
             siteDesignation = 'xs';
             seismicValue = 'A';
-        }
-        else if (document.getElementById('xs-B-option').checked) {
+        } else if (document.getElementById('xs-B-option').checked) {
             siteDesignation = 'xs';
             seismicValue = 'B';
-        }
-        else if (document.getElementById('xs-C-option').checked) {
+        } else if (document.getElementById('xs-C-option').checked) {
             siteDesignation = 'xs';
             seismicValue = 'C';
-        }
-        else if (document.getElementById('xs-D-option').checked) {
+        } else if (document.getElementById('xs-D-option').checked) {
             siteDesignation = 'xs';
             seismicValue = 'D';
-        }
-        else if (document.getElementById('xs-E-option').checked) {
+        } else if (document.getElementById('xs-E-option').checked) {
             siteDesignation = 'xs';
             seismicValue = 'E';
         }
     }
 
+    window.api.invoke('get-token') // Retrieve the token
+        .then((token) => {
+            var myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
+            myHeaders.append("Accept", "application/json");
+            myHeaders.append("Authorization", `Bearer ${token}`);
 
-
-    window.api.invoke('get-token')  // Retrieve the token
-    .then((token) => {
-        var myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json");
-        myHeaders.append("Accept", "application/json");
-        myHeaders.append("Authorization", `Bearer ${token}`);
-
-        var raw = JSON.stringify({
-            "address": `${address}`,
-            "site_designation": `${siteDesignation}`,
-            "seismic_value": `${seismicValue}`
-        });
-
-        var requestOptions = {
-            method: 'POST',
-            headers: myHeaders,
-            body: raw,
-            redirect: 'follow'
-        };
-
-        fetch("http://localhost:42613/location", requestOptions)
-            .then(response => response.json())
-            .then(result => {
-                document.getElementById('wind-velocity-pressure').textContent = result.wind_velocity_pressure;
-                document.getElementById('ground-snow-load').textContent = result.snow_load;
-                document.getElementById('rain-load').textContent = result.rain_load;
-                document.getElementById('design-spectral-acceleration-0-2').textContent = result.design_spectral_acceleration_0_2;
-                document.getElementById('design-spectral-acceleration-1').textContent = result.design_spectral_acceleration_1;
-
-                // set the map as long as we have a valid latitude and longitude
-                if (result.latitude && result.longitude)
-                {
-                    setMap(result.latitude, result.longitude, result.address);
-                }
-
-                else
-                {
-                    setMap(-70.73964, -8.91217, 'unknown');
-                }
-
-                document.getElementById('location_button').disabled = false;
-
-            })
-            .catch(error => {
-                console.log('error', error);
-            })
-            .finally(() => {
-                document.getElementById('location_button').disabled = false;
+            var raw = JSON.stringify({
+                "address": `${address}`,
+                "site_designation": `${siteDesignation}`,
+                "seismic_value": `${seismicValue}`
             });
-    });
+
+            var requestOptions = {
+                method: 'POST',
+                headers: myHeaders,
+                body: raw,
+                redirect: 'follow'
+            };
+
+            fetch("http://localhost:42613/location", requestOptions)
+                .then(response => response.json())
+                .then(result => {
+                    document.getElementById('wind-velocity-pressure').textContent = result.wind_velocity_pressure;
+                    document.getElementById('ground-snow-load').textContent = result.snow_load;
+                    document.getElementById('rain-load').textContent = result.rain_load;
+                    document.getElementById('design-spectral-acceleration-0-2').textContent = result.design_spectral_acceleration_0_2;
+                    document.getElementById('design-spectral-acceleration-1').textContent = result.design_spectral_acceleration_1;
+
+                    // set the map as long as we have a valid latitude and longitude
+                    if (result.latitude && result.longitude) {
+                        setMap(result.latitude, result.longitude, result.address);
+                    } else {
+                        setMap(-70.73964, -8.91217, 'unknown');
+                    }
+
+                    document.getElementById('location_button').disabled = false;
+
+                })
+                .catch(error => {
+                    console.log('error', error);
+                })
+                .finally(() => {
+                    document.getElementById('location_button').disabled = false;
+                });
+        });
 });
