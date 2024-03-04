8092af376051ef1520e03d374ed6498a1f11232a
noahsub <48663769+noahsub@users.noreply.github.com>
2024-03-04 18:40:25 -0500

    Feature: Implemented environment variable check
    More Details:
    - System administrator can input initial values to the .env files without manually creating them and placing them into the appropriate directories

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import secrets
+from pathlib import Path
+
 import uvicorn
 from fastapi import FastAPI
 
@@ -16,6 +19,7 @@ from backend.API.Endpoints.snow_load_endpoint import snow_load_router
 from backend.API.Endpoints.user_data_endpoint import user_data_router
 from backend.API.Endpoints.wall_load_combination_endpoint import wall_load_combination_router
 from backend.API.Endpoints.wind_load_endpoint import wind_load_router
+from config import get_file_path
 
 app = FastAPI()
 app.include_router(authentication_router)
@@ -35,4 +39,54 @@ app.include_router(roof_load_combination_router)
 app.include_router(server_status_endpoint)
 
 if __name__ == "__main__":
+    api_env_path = Path(get_file_path('data/EnvironmentVariables/.env'))
+    database_env_path = Path(get_file_path('database/.env'))
+
+    api_env_valid = True
+    database_env_valid = True
+
+    # check that the files at the paths
+    if not api_env_path.exists():
+        # create the file
+        api_env_path.touch()
+        api_env_valid = False
+
+    else:
+        # get the keys of the .env file
+        with open(api_env_path, 'r') as file:
+            keys = {x.split('=')[0] for x in file.read().splitlines()}
+            if keys != {'API_SECRET_KEY'}:
+                api_env_valid = False
+
+    if not database_env_path.exists():
+        # create the file
+        database_env_path.touch()
+        api_env_valid = False
+
+    else:
+        # get the keys of the .env file
+        with open(database_env_path, 'r') as file:
+            keys = {x.split('=')[0] for x in file.read().splitlines()}
+            if keys != {'ADMIN_PASSWORD', 'ADMIN_USERNAME', 'WRITE_USERNAME', 'READ_PASSWORD', 'WRITE_PASSWORD', 'HOST',
+                        'READ_USERNAME', 'PORT'}:
+                api_env_valid = False
+
+    if api_env_valid is False:
+        # populate the .env file
+        with open(api_env_path, 'w') as file:
+            file.write(f'API_SECRET_KEY={secrets.token_urlsafe(32)}\n')
+
+    if database_env_valid is False:
+        # populate the .env file
+        with open(database_env_path, 'w') as file:
+            print("Please enter the following information associated with the database.")
+            file.write(f'ADMIN_USERNAME={input("Database Admin Username: ")}\n')
+            file.write(f'ADMIN_PASSWORD={input("Database Admin Password: ")}\n')
+            file.write(f'WRITE_USERNAME={input("Database Write Username: ")}\n')
+            file.write(f'WRITE_PASSWORD={input("Database Write Password: ")}\n')
+            file.write(f'READ_USERNAME={input("Database Read Username")}\n')
+            file.write(f'READ_PASSWORD={input("Database Read Password")}\n')
+            file.write(f'HOST={input("Database IP Address (HTTPS is Not Supported Here): ")}\n')
+            file.write(f'PORT={input("Database Port: ")}')
+
     uvicorn.run(app, host="0.0.0.0", port=42613)
