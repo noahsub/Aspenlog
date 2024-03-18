16400a544bb23b4067fa99426447cec271ce7308
noahsub <48663769+noahsub@users.noreply.github.com>
2024-03-18 18:15:21 -0400

    Bug Fix: Installation script was missing sudo in some places

--- linux_server_install.sh
+++ linux_server_install.sh
@@ -1,9 +1,3 @@
-# Check that user is in the SEEDA directory
-if [ ! -d "SEEDA" ]; then
-    echo "Please run this script from the SEEDA directory of the cloned repository"
-    exit 1
-fi
-
 # Add Docker's official GPG key:
 sudo apt-get update
 sudo apt-get install ca-certificates curl
@@ -22,13 +16,15 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin 
 
 sudo docker pull postgres:11.22-bullseye
 
-ehco "Please enter the password you would like to use for the database"
-read -s POSTGRES_PASSWORD
+echo ""
+ehco "Please enter the password you would like to use for the database:"
+read POSTGRES_PASSWORD
 
-echo "Please enter the port you would like to use for the database"
+echo ""
+echo "Please enter the port you would like to use for the database:"
 read POSTGRES_PORT
 
-docker run --name aspenlog2020-database -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD -p $POSTGRES_PORT:5432 -d postgres:11.22-bullseye
+sudo docker run --name aspenlog2020-database -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD -p $POSTGRES_PORT:5432 -d postgres:11.22-bullseye
 
 # Wait for the Postgres service to start running in the Docker container
 echo "Waiting for Postgres service to start..."
