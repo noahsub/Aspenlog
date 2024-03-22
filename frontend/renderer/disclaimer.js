17d7aff5f697cc2d7743367083e1f1ce51d99d75
noahsub <48663769+noahsub@users.noreply.github.com>
2024-03-21 20:16:43 -0400

    Feature: Added disclaimer and license to program, user now needs to accept them before making an account.

---
+++ frontend/renderer/disclaimer.js
@@ -0,0 +1,12 @@
+document.getElementById('accept-button').addEventListener('click', function (event) {
+    // check that disclaimer scroll has been scrolled to the bottom
+    let disclaimerScroll = document.getElementById('disclaimer-scroll');
+    if (disclaimerScroll.scrollTop + disclaimerScroll.clientHeight !== disclaimerScroll.scrollHeight) {
+        alert('Please scroll to the bottom before accepting to ensure you have read the entire contents.');
+        return;
+    }
+
+    console.log('accept-button clicked');
+    window.location.href = 'login.html';
+});
+
