52f3f82befe2d00110de486c8b0699e4458caa7f
noahsub <48663769+noahsub@users.noreply.github.com>
2024-03-24 02:16:17 -0400

    Bug Fix: Removed scroll to bottom check in disclaimer as it was causing issues on different resolutions

--- frontend/renderer/disclaimer.js
+++ frontend/renderer/disclaimer.js
@@ -1,18 +1,7 @@
 document
   .getElementById("accept-button")
-  .addEventListener("click", function (event) {
-    // check that disclaimer scroll has been scrolled to the bottom
-    let disclaimerScroll = document.getElementById("disclaimer-scroll");
-    if (
-      disclaimerScroll.scrollTop + disclaimerScroll.clientHeight !==
-      disclaimerScroll.scrollHeight
-    ) {
-      alert(
-        "Please scroll to the bottom before accepting to ensure you have read the entire contents.",
-      );
-      return;
-    }
-
+  .addEventListener("click", function (event)
+  {
     console.log("accept-button clicked");
     window.location.href = "register.html";
   });
